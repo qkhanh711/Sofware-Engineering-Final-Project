1370334ebfbc685cd87c56cf1387600cf6299f85
qkhanh711 <quockhanh07112003@gmail.com>
2023-07-02 09:44:45 +0700

    data loader

---
+++ config.py
@@ -0,0 +1,20 @@
+import torch
+
+START_TRAIN_AT_IMG_SIZE = 4
+DATASET = '../../Celeba_dataset/img_align_celeba'
+PATH_COLAB = '/content/Celeba_dataset/img_align_celeba'
+CHECKPOINT_GEN = "weightTest/generator.pth"
+CHECKPOINT_CRITIC = "weightTest/critic.pth"
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+SAVE_MODEL = True
+LOAD_MODEL = False
+LEARNING_RATE = 1e-3
+BATCH_SIZES = [32, 32, 32, 16, 16, 16, 16, 8, 4]
+CHANNELS_IMG = 3
+Z_DIM = 256  # 512 in paper
+IN_CHANNELS = 256  # 512 in paper
+CRITIC_ITERATIONS = 1
+LAMBDA_GP = 10
+PROGRESSIVE_EPOCHS = [30] * len(BATCH_SIZES)
+FIXED_NOISE = torch.randn(8, Z_DIM, 1, 1).to(DEVICE)
+NUM_WORKERS = 4
--- data.py
+++ data.py
@@ -1,12 +1,31 @@
 from torch.utils.data import DataLoader
 from torchvision import datasets
 import torchvision.transforms as transforms
+import config
+from math import log2
 
 
-# TRANSFORMS
-transforms = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-# DATALOADER
+def get_loader(image_size, root_path):
+    transform = transforms.Compose([     
+        transforms.Resize((image_size, image_size)),                                                                              
+        transforms.ToTensor(),
+        transforms.Normalize([0.5 for _ in range(config.CHANNELS_IMG)], [0.5 for _ in range(config.CHANNELS_IMG)],),]
+    )
+    batch_size = config.BATCH_SIZES[int(log2(image_size / 4))]
+    dataset = datasets.ImageFolder(root_path, transform=transform)
 
+    dataset = datasets.CIFAR10(root='data', train=True,
+                                    download=True, transform=transform)
+
+    loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=config.NUM_WORKERS,
+        pin_memory=True,
+    )
+    return loader, dataset
+
+if __name__ == '__main__':
+    loader, dataset = get_loader(4 * 2 ** 1)
