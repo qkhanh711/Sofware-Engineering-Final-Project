7c7ba09b5a4ad25aa75990113204a1c4d7e75dfe
qkhanh711 <91941803+qkhanh711@users.noreply.github.com>
2023-07-17 17:01:37 +0700

    upcode

--- Generate.py
+++ Generate.py
@@ -85,8 +85,8 @@ def generate_with_pretrained_model(name, prompt, url = None):
         # subprocess.run(["rm", "-rf", "pretrained/GFPGAN/results"], cwd="Sofware-Engineering-Final-Project")
         subprocess.run(command, cwd="../Sofware-Engineering-Final-Project")
         # subprocess.run(["ls", "pretrained/GFPGAN/results/cmp"], cwd="../Sofware-Engineering-Final-Project/")
-        # path = "/Generate_images/GFPGAN/gfp.png"
-        path = "/Generate_images/GFPGAN/gfp_00.png"
+        path = "/Generate_images/GFPGAN/gfp.png"
+        # path = "/Generate_images/GFPGAN/gfp_00.png"
         return path 
     else:
         if name == "timbrooks/instruct-pix2pix":
